703eb0021b6c80a15dbe69a885a40b42c173b1c7
Marshall Jones <marshalljones@gmail.com>
2015-07-23 08:52:57 -0400

    added observer to update infinite scroll shots

--- extension/js/dribblehd.js
+++ extension/js/dribblehd.js
@@ -16,6 +16,7 @@
 	].concat(sidebars);
 	var isSidebar = h.matches(pathname, sidebars);
 	var shots = h.toArray(document.querySelectorAll('#main ol.dribbbles > li'));
+	var totalShots = shots.length;
 	var defaults = {
 		hide_overlay: false
 	};
@@ -38,6 +39,8 @@
 		media: 'screen'
 	}));
 
+	var isObserving = false;
+
 	// Get extension options and proceed
 	chrome.extension.sendRequest({ get: 'options' }, function (options) {
 		// Options
@@ -47,8 +50,44 @@
 		document.body.classList.add(isSidebar ? 'dribhd-sidebar' : 'dribhd');
 
 		// Modify shots
-		shots.forEach(function (shot) {
-			var link     = shot.querySelector('.dribbble-link');
+		processShots(shots, o);
+
+		// observer target node
+		var target = document.querySelector("#main ol.dribbbles");
+
+		// create an observer instance
+		var observer = new MutationObserver(function(mutations) {
+		  	mutations.forEach(function(mutation) {
+		    	if(mutation.type == 'childList' && mutation.addedNodes.length > 0){
+		    		var newShots = document.querySelectorAll('#main ol.dribbbles > li');
+		    		if(newShots.length > totalShots){
+		    			shots = h.toArray(newShots);
+		    			totalShots = shots.length;
+		    			processShots(shots, o);
+					}
+				}
+		  	});
+		});
+		// configuration of the observer:
+		var config = { childList: true, subtree: true};
+
+		if(isObserving == false){
+			// pass in the target node, as well as the observer options
+			observer.observe(target, config);
+			isObserving = true;
+		}
+
+		// later, you can stop observing
+		// observer.disconnect();
+
+	});
+}());
+
+
+function processShots(shots, o){
+	shots.forEach(function (shot) {
+		var link     = shot.querySelector('.dribbble-link');
+		if(link.querySelector('[data-picture]') != null){
 			var hdPath   = link.querySelector('img').src.replace('_teaser','');
 			var newImage = h.createElement('img');
 			var loader   = h.createElement('span', {
@@ -61,7 +100,9 @@
 			// Image loaded handler
 			function imgLoaded() {
 				loader.remove();
-				link.querySelector('[data-picture]').remove();
+				if(link.querySelector('[data-picture]') != null){
+					link.querySelector('[data-picture]').remove();
+				}
 				h.unbind(newImage, 'load error', imgLoaded);
 				link.appendChild(newImage);
 			}
@@ -79,13 +120,6 @@
 					innerText: shot.querySelector('.dribbble-over strong').innerText
 				}));
 			}
-		});
-
-		// Remove overlay stuff
-		if (o.hide_overlay) {
-			h.toArray(document.querySelectorAll('ol.dribbbles .dribbble-over, ol.dribbbles .liked-by')).forEach(function (element) {
-				element.remove();
-			});
 		}
 	});
-}());
+}
